a860dcf7de1852bfffcbbcf0f177e2d4a69a6632
PeterChageth <74102409+PeterChageth@users.noreply.github.com>
2021-06-27 23:00:09 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note() {
+  return (
+   <div className="note">
+     <h1>Javascrpt and React.js</h1>
+     <p>A basic web dev React Js bootcamp conducted by ShapeAI.</p>
+   </div> 
+  );
+}
+
+export default Note;
